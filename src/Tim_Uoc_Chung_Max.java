ead4093fe2faccc99de2dc1636403e3be5777479
ThanhPhamC <112063001+ThanhPhamC@users.noreply.github.com>
2022-09-28 08:45:15 +0700

    Update Tim_Uoc_Chung_Max.java

--- src/Tim_Uoc_Chung_Max.java
+++ src/Tim_Uoc_Chung_Max.java
@@ -25,18 +25,20 @@ public class Tim_Uoc_Chung_Max {
             }
             System.out.println("UCLN la: " + c);
         } else {
-            if (a < b) {
-                for (int i = 1; i < a; i++) {
-                    if (a % i == 0) {
+         if (a < b) {
+                for (int i = 1; i <= a; i++) {
+                    if (a % i == 0 && b%i==0) {
                         c = i;
                     }
-                }
-                System.out.println("UCLN la: " + c);
+
+                } System.out.println("UCLN la: " + c);
+
             } else {
-                for (int i = 1; i < b; i++) {
-                    if (b % i == 0) {
+                for (int i = 1; i <= b; i++) {
+                    if (b % i == 0 && a%i==0) {
                         c = i;
                     }
+
                 }
                 System.out.println("UCLN la: " + c);
             }
